6588c7222efbdb9deae21904373eacd13e01dd62
arianna choza <unachoza@gmail.com>
2020-01-12 01:39:33 -0500

    added sign in and sign up page, setting up forms

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import {Switch, Route} from 'react-router-dom'
 import './App.css';
 import HomePage from './Pages/homepage/Homepage'
 import ShopPage from './Pages/shop/shop.component'
+import Header from './Components/header/Header.component'
+import SignInAndSignUp from './Pages/Sign-in-and-sign-up/Sign-in-and-sign-up.component'
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage}/>
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
    
