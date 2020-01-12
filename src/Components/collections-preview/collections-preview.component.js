314cff20c6738de112249ccb855b629a760afe14
arianna choza <unachoza@gmail.com>
2020-01-12 01:09:04 -0500

    added items and styles

--- src/Components/collections-preview/collections-preview.component.js
+++ src/Components/collections-preview/collections-preview.component.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
+import './collections-preview.styles.scss'
 
 const CollectionsPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
-      {items.map(item => (
-        <div key={item.id}>{item.name}</div>
-      ))}
+      {items
+        .filter((item, i) => i < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
   </div>
 );
 
-
-export default CollectionsPreview
+export default CollectionsPreview;
