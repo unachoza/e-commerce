27fa359914692f2ad534ed39a688fa4a491cd9cf
arianna choza <unachoza@gmail.com>
2020-01-19 12:29:37 -0500

    input syled and added to form

--- src/Components/Sign-In/SignIn.component.js
+++ src/Components/Sign-In/SignIn.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import FormInput from '../form-input/form-input.component'
-import CustomButton from '../custom-button/custom-button.component'
-import './sign-in.styles.scss'
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+import './sign-in.styles.scss';
 
 class SignIn extends Component {
   constructor(props) {
@@ -14,6 +14,7 @@ class SignIn extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
+    this.setState({ email: '', password: '' });
   };
 
   handleChange = event => {
@@ -28,10 +29,8 @@ class SignIn extends Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <input type="email" name="email" value={email} onChange={this.onChange} required />
-          <label>Email</label>
-          <input type="password" name="password" value={password} onChange={this.onChange} required />
-          <label>Password</label>
+          <FormInput type="email" name="email" value={email} handleChange={this.handleChange} required label="email" />
+          <FormInput type="password" name="password" value={password} handleChange={this.handleChange} required label="password" />
           <input type="submit" value="Submit Form" />
         </form>
       </div>
