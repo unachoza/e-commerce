541ea0adfb8790b78329ff6965c91298f1d3d968
arianna choza <unachoza@gmail.com>
2020-01-20 00:38:38 -0500

    store created, first action used with action complete and working, set Current user

--- src/App.js
+++ src/App.js
@@ -6,36 +6,27 @@ import ShopPage from './Pages/shop/shop.component';
 import Header from './Components/header/Header.component';
 import SignInAndSignUp from './Pages/Sign-in-and-sign-up/Sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.action';
+import { connect } from 'react-redux';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null,
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapshot => {
-          this.setState({
-            currentUser: {
-              id: snapshot.id,
-              ...snapshot.data(),
-            },
+          setCurrentUser({
+            id: snapshot.id,
+            ...snapshot.data(),
           });
         });
-      } else {
-        this.setState({currentUser: userAuth})
       }
+      setCurrentUser(userAuth);
     });
-    console.log(this.state)
   }
 
   componentWillUnmount() {
@@ -45,7 +36,7 @@ class App extends Component {
     console.log(this.state);
     return (
       <div className="app">
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
@@ -56,4 +47,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
+import store from './redux/store'
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
