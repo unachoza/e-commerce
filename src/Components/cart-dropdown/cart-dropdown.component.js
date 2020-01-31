a8154842b2dc1b7867ef617aa3ea138a969cba7d
arianna choza <unachoza@gmail.com>
2020-01-30 21:36:59 -0500

    cart dropdown contained nicely with scss

--- src/Components/cart-dropdown/cart-dropdown.component.js
+++ src/Components/cart-dropdown/cart-dropdown.component.js
@@ -1,13 +1,27 @@
+  
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
 
-export const CartDropdown = () => (
+import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
+
+import './cart-dropdown.styles.scss';
+
+export const CartDropdown = ({items}) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      me
+
     </div>
+    <CustomButton>Go to Checkout</CustomButton>
   </div>
 )
 
-
+// const mapStateToProps = () => ({
+//   hidden: 
+// })
 
 export default CartDropdown
---
+++ src/Components/cart-item/cart-item.component.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import CartIcon from '../cart-icon/cart-icon.component';
+import './cart-item.styles.scss'
+import {connect} from 'react-redux'
+
+const CartItem = ({ item: { imageUrl, name, price, quantity } }) => (
+  <div className="cart-item">
+    <img src={imageUrl} alt="" />
+    <div className="item-details">
+      <span className="name">{name}</span>
+      <span className="price">{quantity} x {price}</span>
+    </div>
+  </div>
+);
+
+const mapStateToProps = () => {
+
+}
+
+export default (connect)(mapStateToProps) (CartItem);
