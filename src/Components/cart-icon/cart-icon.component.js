ca4ad077d529f87bb6d83f497a3a8e5907849d7c
arianna choza <unachoza@gmail.com>
2020-01-31 15:53:26 -0500

    cart icon toggle cart dropdown works

--- src/Components/cart-icon/cart-icon.component.js
+++ src/Components/cart-icon/cart-icon.component.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { ReactComponent as ShoppingIcon } from './cart-icon.svg';
-import './cart-icon.styles.scss'
+import './cart-icon.styles.scss';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { connect } from 'react-redux';
 
-const CartIcon = ({toggleCartHidden, itemCount}) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
     <span className="item-count">0</span>
   </div>
 );
 
-export default CartIcon;
+const mapDispatchtoProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
+
+export default connect(null, mapDispatchtoProps)(CartIcon);
