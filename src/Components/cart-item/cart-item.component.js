c819f6370a79895ba8b4d9d6565f7666c8b7ace5
arianna choza <unachoza@gmail.com>
2020-01-31 22:06:50 -0500

    cart utils additemtocart almost working, still not incrementing quantity

--- src/Components/cart-item/cart-item.component.js
+++ src/Components/cart-item/cart-item.component.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import CartIcon from '../cart-icon/cart-icon.component';
-import './cart-item.styles.scss'
-import {connect} from 'react-redux'
+import './cart-item.styles.scss';
+import { connect } from 'react-redux';
+import { withRouter } from 'react';
 
 const CartItem = ({ item: { imageUrl, name, price, quantity } }) => (
   <div className="cart-item">
     <img src={imageUrl} alt="" />
     <div className="item-details">
       <span className="name">{name}</span>
-      <span className="price">{quantity} x {price}</span>
+      <span className="price">
+        {quantity} x {price}
+      </span>
     </div>
   </div>
 );
 
-const mapStateToProps = () => {
+const mapStateToProps = () => {};
 
-}
-
-export default (connect)(mapStateToProps) (CartItem);
+export default connect(mapStateToProps)(CartItem);
