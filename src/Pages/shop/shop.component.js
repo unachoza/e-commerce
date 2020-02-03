a9fe2d763f20aa06d3c841d162b8240406127d41
arianna choza <unachoza@gmail.com>
2020-02-02 22:36:07 -0500

    moving shop  collections data to redux

--- src/Pages/shop/shop.component.js
+++ src/Pages/shop/shop.component.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
-import SHOP_DATA from '../../redux/shop/shop.data';
+import React from 'react';
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+
 import CollectionsPreview from '../../Components/collections-preview/collections-preview.component';
+import {selectCollections} from '../../redux/shop/shop.selectors'
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (<div className="shop-page">
-      {collections.map(({id, ...otherCollectionProps} )=> (
-        <CollectionsPreview key={id} {...otherCollectionProps}/>
-      ))}
-    </div>);
-  }
-}
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionsPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+})
+export default connect(mapStateToProps)(ShopPage);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducers'
 import directoryReducer from './directory/directory.reducer'
+import shopReducer from './shop/shop.reducer'
 import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
@@ -15,7 +16,8 @@ const persistConfig = {
 const roodReducer =  combineReducers({
   user: userReducer,
   cart: cartReducer,
-  directory: directoryReducer
+  directory: directoryReducer,
+  shop: shopReducer
 })
 
 
