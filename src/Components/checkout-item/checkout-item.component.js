86b683fdc4ef4392ff0111b9f01898f5d340f510
arianna choza <unachoza@gmail.com>
2020-02-02 19:22:58 -0500

    fixed checkout item quantity styles

--- src/Components/checkout-item/checkout-item.component.js
+++ src/Components/checkout-item/checkout-item.component.js
@@ -1,40 +1,40 @@
-import React from 'react'
-import './checkout-item.styles.scss'
-import { connect } from 'react-redux'
-import {createStructuredSelector } from 'reselect'
-import { selectCartItems } from '../../redux/cart/cart.selectors'
-import {addItem, removeItem} from '../../redux/cart/cart.actions'
+import React from 'react';
+import './checkout-item.styles.scss';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { addItem, removeItem } from '../../redux/cart/cart.actions';
 
-const CheckoutItem = ({ cartItem , addItem, removeItem}) => {
-  const { name, imageUrl, price, quantity } = cartItem
+const CheckoutItem = ({ cartItem, addItem, removeItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <div className='arrow' onClick={() => removeItem(cartItem)}>
+      <span className="quantity">
+      <div className="arrow" onClick={() => removeItem(cartItem)}>
         &#10094;
-        </div>
-      <span className='value'>{quantity}</span>
-      <div className='arrow' onClick={() => addItem(cartItem)}>
+      </div>
+      <span className="value">{quantity}</span>
+      <div className="arrow" onClick={() => addItem(cartItem)}>
         &#10095;
-        </div>
+      </div>
+      </span>
+     
       <span className="price">{price}</span>
-      <div className="remove-button">
-        &#10005;
-   </div>
+      <div className="remove-button">&#10005;</div>
     </div>
-
-  )
-}
+  );
+};
 // const mapStateToProps = createStructuredSelector({
 // cartItems: selectCartItems
 // })}
 
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item)),
-  removeItem : item => dispatch(removeItem(item))
-})
+  removeItem: item => dispatch(removeItem(item)),
+});
 
-export default connect(null, mapDispatchToProps)(CheckoutItem)
+export default connect(null, mapDispatchToProps)(CheckoutItem);
