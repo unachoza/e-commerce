efea7e69557e978fc5abe760667085335d804377
arianna choza <unachoza@gmail.com>
2020-02-03 00:54:59 -0500

    works

--- src/Pages/collection/collection.component.js
+++ src/Pages/collection/collection.component.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import {  selectCollection } from '../../redux/shop/shop.selectors';
+import { selectCollection } from '../../redux/shop/shop.selectors';
+import CollectionItem from '../../Components/collection-item/collection-item.component';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({collection}) => {
-  console.log(collection)
+const CollectionPage = ({ collection }) => {
+  const { items, title } = collection;
   return (
-  
     <div className="collection-page">
-      <h2>colleciton Plage</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-})
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
 
-export default connect(mapStateToProps)(CollectionPage)
+export default connect(mapStateToProps)(CollectionPage);
