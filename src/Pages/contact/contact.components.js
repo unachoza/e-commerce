a96a0b508fa3e3869aa853d5fb26b15a94bfcaf0
arianna choza <unachoza@gmail.com>
2020-02-03 20:16:43 -0500

    contact page

---
+++ src/Pages/contact/contact.components.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './contact.styles.scss';
+const ContactPage = () => {
+  return (
+    <div className="contact-page">
+      User Feedback? Get intouch!<br></br>
+      unachoza@gmail.com<br></br>
+      <a href="https://www.linkedin/in/arianna-choza" target=" _">
+        LinkedIn
+      </a>
+      <br></br>
+      <a href="https://www.github/unachoza" target=" _">
+        Github
+      </a>
+      <br></br>
+      <a href="https://www.arianna-choza.surge.sh" target=" _">
+        Portfolio
+      </a>
+      <br></br>
+    </div>
+  );
+};
+
+export default ContactPage;
