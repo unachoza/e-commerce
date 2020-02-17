6ff6a5da91b09fc5f69eacf013d189fd6e05db2e
arianna choza <unachoza@gmail.com>
2020-02-16 23:22:26 -0500

    working through loading data bc asynchronus

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,13 +1,12 @@
-import SHOP_DATA from './shop.data';
 import ShopActionTypes from './shop.types';
 
 const INITIAL_STATE = {
-  collections: SHOP_DATA,
+  collections: null,
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case ShopActionTypes.updateCollections:
+    case ShopActionTypes.UPDATE_COLLECTIONS:
       return {
         ...state,
         collections: action.payload,
