535adefe8aaade033f349aadcf928635b4ab073e
arianna choza <unachoza@gmail.com>
2020-02-25 18:25:00 -0500

    changed firestore access rules, loading spinner works bc has access to firestore data

--- src/Pages/shop/shop.component.js
+++ src/Pages/shop/shop.component.js
@@ -4,9 +4,15 @@ import { connect } from 'react-redux';
 import CollectionsOverview from '../../Components/collection-overview/collection-overview.component';
 import CollectionPage from '../collection/collection.component';
 import { updateCollections } from '../../redux/shop/shop.actions';
+import WithSpinner from '../../Components/with-spinner/with-spinner.component';
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 class ShopPage extends Component {
+  state = {
+    loading: true,
+  };
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
@@ -16,15 +22,24 @@ class ShopPage extends Component {
     this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshop => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshop);
       updateCollections(collectionsMap);
+      this.setState({ loading: false });
     });
   }
 
   render() {
     const { match } = this.props;
+    const {loading} = this.state
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          render={props => <CollectionPageWithSpinner isLoading={loading} {...props} />}
+        />
       </div>
     );
   }
