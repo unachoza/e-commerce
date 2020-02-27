d2ab27005e0c686fa9124b164022ac2362fa88bd
arianna choza <unachoza@gmail.com>
2020-02-26 22:47:12 -0500

    tab openning safety

--- src/Pages/contact/contact.components.js
+++ src/Pages/contact/contact.components.js
@@ -5,15 +5,15 @@ const ContactPage = () => {
     <div className="contact-page">
       Get in touch!<br></br>
       unachoza@gmail.com<br></br>
-      <a href="https://www.linkedin.com/in/arianna-choza" target=" _">
+      <a href="https://www.linkedin.com/in/arianna-choza" target=" _" rel="noopener noreferrer">
         LinkedIn
       </a>
       <br></br>
-      <a href="https://www.github.com/unachoza" target=" _">
+      <a href="https://www.github.com/unachoza" target=" _" rel="noopener noreferrer">
         Github
       </a>
       <br></br>
-      <a href="http://www.arianna-choza.surge.sh" target=" _">
+      <a href="http://www.arianna-choza.surge.sh" target=" _" rel="noopener noreferrer">
         Portfolio
       </a>
       <br></br>
