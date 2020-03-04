6c4f4b66df6ccfde760e958fcf8fa5bb7eacb9e8
arianna choza <unachoza@gmail.com>
2020-03-04 14:10:54 -0500

    global styles in preperation for media queries

---
+++ src/global.styles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyles } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyles`
+ body {
+   font-family: 'Open Sans Condensed';
+   padding: 20px 40px;
+ }
+
+ a {
+   text-decoration: none;
+   color: black;
+ }
+
+ * {
+   box-sizing: border-box;
+ }
+
+`;
